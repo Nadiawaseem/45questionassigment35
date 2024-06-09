e692452bd77290045596f7dbd57cd59ef5cc713f
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-08 17:23:38 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+var animal = ["loin", "dog", "rabbit"];
+for (var i = 0; i < animal.length; i++) {
+    console.log('A ${animal[i]} would make a great pet');
+}
+console.log("Any of these animals would make a great pet!|");
---
+++ main.ts
@@ -0,0 +1,7 @@
+   let animal :string[] = ["loin","dog", "rabbit"];
+
+   for(let i = 0 ; i<animal.length ; i++) {
+    console.log('A ${animal[i]} would make a great pet');
+
+   }
+   console.log("Any of these animals would make a great pet!|");
